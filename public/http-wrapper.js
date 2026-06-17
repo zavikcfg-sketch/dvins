d98c0177c860bf080dfdacbc8e18b952b3573bc1
zavikcfg-sketch <zavikcfg@gmail.com>
2026-06-17 19:37:40 +0300

    Delete public directory

--- public/http-wrapper.js
+++
@@ -1,4 +0,0 @@
-// Точка входа для BotHost (запускается из корня /app).
-// Запускает наш единый сервер (сайт + API + Telegram-бот).
-// Сервер сам доустановит express и node-telegram-bot-api, если их нет.
-require("./bot/server.js");
